5a15736ab92adeb1c9b2e3c0fa7254ab447f117e
thomas9911 <thomastimmer11@hotmail.com>
2018-11-19 13:20:43 +0100

    Added url in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from distutils.core import setup
 
+with open('README') as f:
+    long_description = f.read()
+
 setup(
     name='FunctionalCache',
     description='SQLite function cache',
     version='0.1',
     packages=['functional_cache', ],
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
+    url='https://github.com/thomas9911/FunctionalCache',
+    author='thomas9911',
+    author_email='thomastimmer11@hotmail.com'
 )
